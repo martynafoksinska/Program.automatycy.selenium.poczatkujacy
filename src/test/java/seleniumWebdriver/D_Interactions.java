a9feb4b55ed759e2cf6a442ff4c59e3ee98b0236
martynafoksinska <martyna.a.foksinska@gmail.com>
2022-09-08 13:17:30 +0200

    interactions exercise done

--- src/test/java/seleniumWebdriver/D_Interactions.java
+++ src/test/java/seleniumWebdriver/D_Interactions.java
@@ -5,7 +5,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.time.Duration;
 import java.util.List;
 
 public class D_Interactions {
@@ -13,7 +15,7 @@ public class D_Interactions {
     public WebDriver getDriver() {
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
-        driver.navigate().to("https://programautomatycy.pl/test-page/");
+        driver.navigate().to("https://programautomatycy.pl/courses/");
         return driver;
     }
 
@@ -39,6 +41,12 @@ public class D_Interactions {
         WebElement descriptionTextByCss = driver.findElement(By.cssSelector("#description-text"));
         descriptionTextByCss.sendKeys("Selenium WebDriver Java dla początkujących");
 
+        WebElement bookTitle = driver.findElement(By.id("book-text"));
+        bookTitle.sendKeys("Harry Potter");
+        WebElement movieTitle = driver.findElement(By.id("movie-text"));
+        movieTitle.sendKeys("Szklana Pułapka");
+
+
         //driver.quit();
     }
 
@@ -47,56 +55,61 @@ public class D_Interactions {
 
         WebDriver driver = getDriver();
 
-        // Poniższa linia akceptuje pobieranie ciasteczek
-        // Możesz zakomentować tę część i nie godzić się na pobieranie ciasteczek, ale wtedy nie będą działały niektóre polecenia na stronie
+//        // Poniższa linia akceptuje pobieranie ciasteczek
+//        // Możesz zakomentować tę część i nie godzić się na pobieranie ciasteczek, ale wtedy nie będą działały niektóre polecenia na stronie
         driver.findElement(By.id("cn-accept-cookie")).click();
-
-        /** Wybierz opcję */
-        WebElement optionByXpath = driver.findElement(By.xpath("//input[@value='Opcja 3']"));
-        optionByXpath.click();
-
-        /** Wybierz ulubiony dzień tygodnia */
-        WebElement dayDropBoxByXpath = driver.findElement(By.xpath("//*[@id='day-select']")).findElement(By.xpath("//*[@value='Poniedziałek']"));
-        dayDropBoxByXpath.click();
-
-        /** Wybierz przeglądarkę */
-        WebElement browserSelectOptionByXpath = driver.findElement(By.xpath("//*[@id='browser-select-multiple']")).findElement(By.xpath("//*[@value='Google Chrome']"));
-        browserSelectOptionByXpath.click();
-
-        /** link - polityka prywatności */
-        WebElement linkByXpath = driver.findElement(By.xpath("//*[text()='polityka prywatności']"));
-        linkByXpath.click();
-
-        // Wróc do poprzedniej strony
-        driver.navigate().back();
-
-        /** Zgadzam się z polityką prywatności - polityka prywatności */
-        WebElement policyRadioButtonsByXpath = driver.findElement(By.xpath("//*[@id='wpcf7-f9259-p9257-o1']//input[@value='Tak']"));
-        policyRadioButtonsByXpath.click();
-
-        /** Zaznacz wszystkie pola z literą "C" */
-//        WebElement cLetterFirstCheckBoxById = driver.findElement(By.name("c1[]"));
-//        cLetterFirstCheckBoxById.click();
-//        WebElement cLetterSecondCheckBoxById = driver.findElement(By.name("c2[]"));
-//        cLetterSecondCheckBoxById.click();
-
-        /** Zaznacz wszystkie pola z literą "C" - Zastosowanie masowego szukania elementów oraz zaznaczenie elementów w pętli */
-        List<WebElement> cLettersCheckBoxByXpath = driver.findElements(By.xpath("//input[@type='checkbox' and @value='C']"));
-        for (WebElement cLetter:cLettersCheckBoxByXpath){
-            cLetter.click();
-        }
-//        for (int i = 0; i < cLettersCheckBoxByXpath.size(); i++){
-//            cLettersCheckBoxByXpath.get(i).click();
+//
+//        /** Wybierz opcję */
+//        WebElement optionByXpath = driver.findElement(By.xpath("//input[@value='Opcja 3']"));
+//        optionByXpath.click();
+//
+//        /** Wybierz ulubiony dzień tygodnia */
+//        WebElement dayDropBoxByXpath = driver.findElement(By.xpath("//*[@id='day-select']")).findElement(By.xpath("//*[@value='Poniedziałek']"));
+//        dayDropBoxByXpath.click();
+//
+//        /** Wybierz przeglądarkę */
+//        WebElement browserSelectOptionByXpath = driver.findElement(By.xpath("//*[@id='browser-select-multiple']")).findElement(By.xpath("//*[@value='Google Chrome']"));
+//        browserSelectOptionByXpath.click();
+//
+//        /** link - polityka prywatności */
+//        WebElement linkByXpath = driver.findElement(By.xpath("//*[text()='polityka prywatności']"));
+//        linkByXpath.click();
+//
+//        // Wróc do poprzedniej strony
+//        driver.navigate().back();
+//
+//        /** Zgadzam się z polityką prywatności - polityka prywatności */
+//        WebElement policyRadioButtonsByXpath = driver.findElement(By.xpath("//*[@id='wpcf7-f9259-p9257-o1']//input[@value='Tak']"));
+//        policyRadioButtonsByXpath.click();
+//
+//        /** Zaznacz wszystkie pola z literą "C" */
+////        WebElement cLetterFirstCheckBoxById = driver.findElement(By.name("c1[]"));
+////        cLetterFirstCheckBoxById.click();
+////        WebElement cLetterSecondCheckBoxById = driver.findElement(By.name("c2[]"));
+////        cLetterSecondCheckBoxById.click();
+//
+//        /** Zaznacz wszystkie pola z literą "C" - Zastosowanie masowego szukania elementów oraz zaznaczenie elementów w pętli */
+//        List<WebElement> cLettersCheckBoxByXpath = driver.findElements(By.xpath("//input[@type='checkbox' and @value='C']"));
+//        for (WebElement cLetter:cLettersCheckBoxByXpath){
+//            cLetter.click();
 //        }
+////        for (int i = 0; i < cLettersCheckBoxByXpath.size(); i++){
+////            cLettersCheckBoxByXpath.get(i).click();
+////        }
+//
+//        /** Wybierz grupę */
+//        WebElement groupMultipleCheckBoxByXpath = driver.findElement(By.xpath("//*[@id='group-checkbox']/span[3]/input"));
+//        groupMultipleCheckBoxByXpath.click();
+//
+//        /** Naciśnij przycisk - NIC NIE RÓB */
+//        WebElement doNothingButtonById = driver.findElement(By.id("nothing-submit"));
+//        doNothingButtonById.click();
 
-        /** Wybierz grupę */
-        WebElement groupMultipleCheckBoxByXpath = driver.findElement(By.xpath("//*[@id='group-checkbox']/span[3]/input"));
-        groupMultipleCheckBoxByXpath.click();
+        WebElement number3 = driver.findElement(By.xpath("//*[@id='number-radio']/span[3]")).findElement(By.xpath("//*[@id='number-radio']/span[3]/input"));
+        number3.click();
 
-        /** Naciśnij przycisk - NIC NIE RÓB */
-        WebElement doNothingButtonById = driver.findElement(By.id("nothing-submit"));
-        doNothingButtonById.click();
-
+        WebElement greenColour = driver.findElement(By.xpath("//*[@id='colour-select-multiple']")).findElement(By.xpath("//*[@id='colour-select-multiple']/option[3]"));
+        greenColour.click();
         //driver.quit();
     }
 
@@ -113,7 +126,7 @@ public class D_Interactions {
         System.out.println("###########################");
 
         // Pobierz i wyświetl adres URL strony.
-        System.out.println(driver.getCurrentUrl());
+//        System.out.println(driver.getCurrentUrl());
 
         System.out.println("###########################");
 
@@ -123,7 +136,7 @@ public class D_Interactions {
         System.out.println("###########################");
 
         // Pobierz i wyświetl źródło kodu całej strony
-        System.out.println(driver.getPageSource());
+//        System.out.println(driver.getPageSource());
 
         driver.quit();
     }
