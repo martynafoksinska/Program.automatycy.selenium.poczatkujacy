c2de6caabed680670cbc9671627f18ef38e157a9
martynafoksinska <martyna.a.foksinska@gmail.com>
2022-09-09 12:52:42 +0200

    Assertions exercise done

--- src/test/java/seleniumWebdriver/I_Assertions.java
+++ src/test/java/seleniumWebdriver/I_Assertions.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.By;
 import org.junit.Assert;
+import org.openqa.selenium.support.ui.Select;
 
 public class I_Assertions {
 
@@ -20,29 +21,45 @@ public class I_Assertions {
     public void assertOptionsPassed() {
 
         WebDriver driver = getDriver();
+        driver.findElement(By.id("cn-accept-cookie")).click();
 
-        /** Wpisz imię */
-        // Potwierdź, że wpisane imie zgadza się z wyświetlanym tekstem
-        WebElement firstNameFieldById = driver.findElement(By.id("firstname-text"));
-        firstNameFieldById.sendKeys("Jan");
-        Assert.assertEquals("Jan", firstNameFieldById.getAttribute("value"));
+        WebElement someFieldByXpath = driver.findElement(By.xpath("//*[@id='text-text']"));
+        Assert.assertTrue(someFieldByXpath.isDisplayed());
 
-        /** Wpisz nazwisko */
-        // Potwierdź, że pole z nazwiskiem jest edytowalne
-        WebElement lastNameFieldByName = driver.findElement(By.name("your-lastname"));
-        Assert.assertTrue(lastNameFieldByName.isEnabled());
+        Select multipleFieldColour = new Select(driver.findElement(By.id("colour-select-multiple")));
+        multipleFieldColour.selectByValue("Czarny");
+        Assert.assertEquals("Czarny", multipleFieldColour.getFirstSelectedOption().getText());
 
-        /** Przycisk pojawiający się z opóźnieniem: 2 sekundy */
-        // Potwierdź, że przycisk pojawiający się z opóźnieniem jest niewidoczny na stronie
-        WebElement delayButtonById = driver.findElement(By.id("delay-button"));
-        Assert.assertFalse(delayButtonById.isDisplayed());
+        WebElement number3 = driver.findElement(By.xpath("//span[@id='number-radio']")).findElement(By.xpath("//*[@value='3']"));
 
-        /** Nieedytowalny checkbox */
-        // Potwierdź, że atrybut "disabled" w obiekcie disabledCheckBoxById nie jest nulem
-        WebElement disabledCheckBoxById = driver.findElement(By.id("disabled-checkbox"));
-        Assert.assertNotNull(disabledCheckBoxById.getAttribute("disabled"));
+        number3.click();
 
-        //driver.quit();
+        Assert.assertTrue(number3.isSelected());
+
+
+
+//        /** Wpisz imię */
+//        // Potwierdź, że wpisane imie zgadza się z wyświetlanym tekstem
+//        WebElement firstNameFieldById = driver.findElement(By.id("firstname-text"));
+//        firstNameFieldById.sendKeys("Jan");
+//        Assert.assertEquals("Jan", firstNameFieldById.getAttribute("value"));
+//
+//        /** Wpisz nazwisko */
+//        // Potwierdź, że pole z nazwiskiem jest edytowalne
+//        WebElement lastNameFieldByName = driver.findElement(By.name("your-lastname"));
+//        Assert.assertTrue(lastNameFieldByName.isEnabled());
+//
+//        /** Przycisk pojawiający się z opóźnieniem: 2 sekundy */
+//        // Potwierdź, że przycisk pojawiający się z opóźnieniem jest niewidoczny na stronie
+//        WebElement delayButtonById = driver.findElement(By.id("delay-button"));
+//        Assert.assertFalse(delayButtonById.isDisplayed());
+//
+//        /** Nieedytowalny checkbox */
+//        // Potwierdź, że atrybut "disabled" w obiekcie disabledCheckBoxById nie jest nulem
+//        WebElement disabledCheckBoxById = driver.findElement(By.id("disabled-checkbox"));
+//        Assert.assertNotNull(disabledCheckBoxById.getAttribute("disabled"));
+//
+//        //driver.quit();
     }
 
     @Test
